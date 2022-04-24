04faf54d44c971cdcec42416d008461a09d45e43
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-04-24 17:48:01 +0100

    commit

--- diagram.py
+++ diagram.py
@@ -4,13 +4,13 @@ from diagrams.aws.compute import EC2AutoScaling, EC2Instance
 from diagrams.aws.database import RDS
 
 with Diagram("Diagram", direction="TB", show=False):
-  with Cluster("AWS"):
-    IGW = InternetGateway("IGW")
-    with Cluster("VPC"):
-      LB = ALB("LB")
-      ASG = EC2AutoScaling("ASG")
-      with Cluster("EC2"): 
-        EC2Instance = [EC2Instance("nginx+ghost AZ1"), EC2Instance("nginx+ghost AZ2")]
-      with Cluster("RDS"):
-        RDS = [RDS("mysql AZ1"), RDS("mysql AZ2")]
-    IGW >> LB >> ASG >> EC2Instance[0] >> RDS[0]
+    with Cluster("AWS"):
+        IGW = InternetGateway("IGW")
+        with Cluster("VPC"):
+            LB = ALB("LB")
+            ASG = EC2AutoScaling("ASG")
+            with Cluster("EC2"):
+                EC2Instance = [EC2Instance("nginx+ghost AZ1"), EC2Instance("nginx+ghost AZ2")]
+            with Cluster("RDS"):
+                RDS = [RDS("mysql AZ1"), RDS("mysql AZ2")]
+        IGW >> LB >> ASG >> EC2Instance[0] >> RDS[0]
