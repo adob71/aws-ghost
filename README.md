6e3c0ef7cf37549cb348f80102cbaa3ce6830f5a
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-04-25 18:15:07 +0100

    commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # nordcloud
+
+AWS
+VPC
+CloudFront
+InternetGateway
+ALB
+EC2AutoScaling
+EC2 Instance AZ1
+EC2 Standby AZ2
+RDS
+DB Instance AZ1
+DB Standby AZ2
+CloudFront >> InternetGateway >> ALB >> EC2Instance[0] >> RDS[0]
+
+CloudWatch
+EC2Instance[0] >> Cloudwatch
+
+CodePipeline
+CodeCommit
+CodeBuild
+CodeDeploy
+Codecommit >> Codebuild >> Codedeploy >> EC2Instance[0]
+
--- diagram.py
+++ diagram.py
@@ -1,5 +1,5 @@
 from diagrams import Diagram, Cluster
-from diagrams.aws.network import VPC, InternetGateway, ALB
+from diagrams.aws.network import VPC, CloudFront, InternetGateway, ALB
 from diagrams.aws.compute import EC2AutoScaling, EC2Instance
 from diagrams.aws.database import RDS
 from diagrams.aws.devtools import Codepipeline, Codecommit, Codebuild, Codedeploy
@@ -11,22 +11,21 @@ with Diagram("Diagram", direction="LR", show=False):
     with Cluster("AWS"):
 
         with Cluster("VPC"):
+            CloudFront = CloudFront("CloudFront")
             InternetGateway = InternetGateway("InternetGateway")
             ALB = ALB("ALB")
-            EC2AutoScaling = EC2AutoScaling("EC2AutoScaling")
-            with Cluster("EC2Instance"):
+            with Cluster("EC2AutoScaling"):
                 EC2Instance = [EC2Instance("EC2 Instance AZ1"), EC2Instance("EC2 Standby AZ2")]
             with Cluster("RDS"):
                 RDS = [RDS("DB Instance AZ1"), RDS("DB Standby AZ2")]      
-            InternetGateway >> ALB >> EC2AutoScaling >> EC2Instance[0] >> RDS[0]
+            CloudFront >> InternetGateway >> ALB >> EC2Instance[0] >> RDS[0]
+
+        with Cluster("CloudWatch"):
+            Cloudwatch = Cloudwatch("CloudWatch")
+            EC2Instance[0] >> Cloudwatch
 
         with Cluster("CodePipeline"):
             Codecommit = Codecommit("CodeCommit")
             Codebuild = Codebuild("CodeBuild")
             Codedeploy = Codedeploy("CodeDeploy")
             Codecommit >> Codebuild >> Codedeploy >> EC2Instance[0]
-
-        with Cluster("CloudWatch"):
-            Cloudwatch = Cloudwatch("CloudWatch")
-            EC2Instance[0] >> Cloudwatch
-
