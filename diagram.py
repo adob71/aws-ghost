aacb63101897f5cbf2b9c4b6699ba2e26c9a687d
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-04-26 14:51:40 +0100

    commit

--- diagram.py
+++ diagram.py
@@ -4,8 +4,12 @@ from diagrams.aws.compute import EC2AutoScaling, EC2Instance
 from diagrams.aws.database import RDS
 from diagrams.aws.devtools import Codepipeline, Codecommit, Codebuild, Codedeploy
 from diagrams.aws.management import OrganizationsAccount, Cloudwatch
+from diagrams.aws.general import Users
 
 with Diagram("Diagram", direction="LR", show=False):
+
+    DevOps = Users("DevOps")
+    Users = Users("Users")
 
     with Cluster("AWS"):
 
@@ -19,13 +23,13 @@ with Diagram("Diagram", direction="LR", show=False):
                     EC2Instance = [EC2Instance("EC2 Instance AZ1"), EC2Instance("EC2 Standby AZ2")]
                 with Cluster("RDS"):
                     RDS = [RDS("DB Instance AZ1"), RDS("DB Standby AZ2")]      
-                CloudFront >> InternetGateway >> ALB >> EC2Instance[0] >> RDS[0]
+                Users >> CloudFront >> InternetGateway >> ALB >> EC2Instance[0] >> RDS[0]
 
             with Cluster("CodePipeline"):
                 Codecommit = Codecommit("CodeCommit")
                 Codebuild = Codebuild("CodeBuild")
                 Codedeploy = Codedeploy("CodeDeploy")
-                Codecommit >> Codebuild >> Codedeploy >> EC2Instance[0]
+                DevOps >> Codecommit >> Codebuild >> Codedeploy >> EC2Instance[0]
 
             with Cluster("CloudWatch"):
                 Cloudwatch = Cloudwatch("CloudWatch")
