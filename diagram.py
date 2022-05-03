3627ccee7d929148471c0adc58136d930c0873b6
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-05-03 16:01:40 +0100

    commit

--- diagram.py
+++ diagram.py
@@ -19,20 +19,26 @@ with Diagram("Diagram", direction="LR", show=False):
                 CloudFront = CloudFront("(1) CloudFront")
                 InternetGateway = InternetGateway("(2) InternetGateway")
                 ALB = ALB("(3) ALB")
+
                 with Cluster("EC2AutoScaling"):
                     EC2Instance = [EC2Instance("(4) EC2 Instance AZ1"), EC2Instance("(5) EC2 Standby AZ2")]
 #                EC2Instance[0] >> Edge(color="red", style="dashed", label="Failover") >> EC2Instance[1]
+
                 with Cluster("RDS"):
                     RDS = [RDS("(6) DB Instance AZ1"), RDS("(7) DB Standby AZ2")]      
 #                RDS[0] >> Edge(color="red", style="dashed", label="Replicate") >> RDS[1]
+
                 Users >> CloudFront >> InternetGateway >> ALB >> EC2Instance[0] >> RDS[0]
 
             with Cluster("CloudWatch"):
                 Cloudwatch = Cloudwatch("(8) CloudWatch")
+
                 EC2Instance[0] >> Cloudwatch
+                RDS[0] >> Cloudwatch
 
             with Cluster("CodePipeline"):
                 Codecommit = Codecommit("(9) CodeCommit")
                 Codebuild = Codebuild("(10) CodeBuild")
                 Codedeploy = Codedeploy("(11) CodeDeploy")
+
                 DevOps >> Codecommit >> Codebuild >> Codedeploy >> EC2Instance[0]
