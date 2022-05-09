f6a05e1e08efb97af9bf45494dd083be18e4d64a
ec2-user <ec2-user@ip-172-31-1-45.eu-west-2.compute.internal>
2022-05-09 06:58:34 +0100

    commit

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ exports.handler = async (event) => {
 const GhostAdminAPI = require('@tryghost/admin-api');
 
 const api = new GhostAdminAPI({
-  url: 'http://ghost-lb-1239055692.eu-west-2.elb.amazonaws.com',
-  key: '62778ff7672f5c156f79df28:eebd3871263a0287c507423704da0310c6754dc48ecdd7df2faaeec3a93b3099',
+  url: event.key1,
+  key: event.key2,
   version: "v3.0"
 });
 
-api.posts
+await api.posts
     .browse({limit: "all" })
     .then((posts) => {
         posts.forEach((post) => {
@@ -19,8 +19,8 @@ api.posts
     
 const response = {
     statusCode: 200,
-    body: JSON.stringify('Success'),
+    body: JSON.stringify('Exit'),
 };
 
 return response;
-}
+};
